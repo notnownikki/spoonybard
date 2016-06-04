eb69f97c5a1a30edd0b4d37a45f63d876a27147e
Nicola Heald <nicola@notnowlewis.com>
2016-06-04 21:43:02 +0100

    First integration test, runa a job from yaml locally

--- spoonybard/core/executors.py
+++ spoonybard/core/executors.py
@@ -1,5 +1,26 @@
+import tempfile
+import os
+import subprocess
+
+
 class Executor(object):
-    pass
+    def __init__(self, config={}):
+        self.config = config
+        self.log = []
+
+    def run(self, script):
+        self.setup_stream(script)
+        line = self.stream.readline()
+        while line:
+            self.log.append(line)
+            line = self.stream.readline()
+        return self.get_exit_code()
+
+    def get_log(self):
+        return self.log
+
+    def get_exit_code(self):
+        return None
 
 
 class SSHExecutor(Executor):
@@ -7,4 +28,19 @@ class SSHExecutor(Executor):
 
 
 class LocalExecutor(Executor):
-    pass
+    def setup_stream(self, script):
+        fd, tmp_script_filename = tempfile.mkstemp()
+        os.close(fd)
+        f = open(tmp_script_filename, 'w')
+        f.write(script)
+        f.close()
+        os.chmod(tmp_script_filename, 0o744)
+        p = subprocess.Popen(
+            tmp_script_filename, shell=True, stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+        self.process = p
+        self.stream = p.stdout
+
+    def get_exit_code(self):
+        self.process.wait()
+        return self.process.returncode
